e46f77306ee6fe1d707002a6bff24f104240a01e
Craig Tyler Celestin <crgcelestin@gmail.com>
2024-02-06 14:43:48 -0500

    finally able to get exams

--- client/src/pages/exams.js
+++ client/src/pages/exams.js
@@ -1,11 +1,40 @@
+import React, { useState, useEffect } from "react";
+
 const Exams_Page = () => {
+
+    const [exams, setExams] = useState([]);
+
+    useEffect(() => {
+        const fetchExams = async () => {
+            try {
+                const response = await fetch('https://czi-covid-lypkrzry4q-uc.a.run.app/api/exams')
+                const res = await response.json();
+                const exams_data = res["exams"]
+                setExams(exams_data)
+            } catch (e) {
+                console.error(e)
+            }
+        }
+        fetchExams();
+    }, []);
+
     return (
-        <div>
-            <h2>
-                Patient Exams
-            </h2>
-        </div>
-    )
+        <>
+            <div>
+                <h2>Patient Exams</h2>
+            </div>
+            <div>
+                <h1>List All Exams</h1>
+                {exams.map(exam => (
+                    exam && (
+                        <div key={exam.patientId}>
+                            {exam.age}
+                        </div>
+                    )
+                ))}
+            </div>
+        </>
+    );
 }
 
 export default Exams_Page;
